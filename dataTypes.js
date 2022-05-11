83f43d1fe208838ae309596d6b0935663f2d91af
Ashkhen-Adamyan <salaxyan1989@mail.ru>
2022-05-11 22:43:00 +0300

    add lesson

---
+++ dataTypes.js
@@ -0,0 +1,23 @@
+const dataTypeNumber = 123456                 // number
+console.log(typeof dataTypeNumber);
+
+const dataTypeInfinity = Infinity             // number
+console.log(typeof dataTypeInfinity);
+
+console.log(typeof NaN);                      // number
+
+const dataTypeString = 'string'               // string 
+console.log(typeof dataTypeString);
+
+const dataTypeBooleanTrue = true             // boolean
+console.log(typeof dataTypeBooleanTrue);
+
+const dataTypeBooleanFalse = false           // boolean
+console.log(typeof dataTypeBooleanFalse);
+
+console.log(typeof null);                     // object
+
+const dataTypeArray = [1, 2, 3, 4, 5, 6]      // object
+console.log(typeof dataTypeArray);
+
+console.log(typeof undefined);                // undefined
